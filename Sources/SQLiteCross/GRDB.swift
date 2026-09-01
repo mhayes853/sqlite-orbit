0ec410c0389972272bc2c26d46d825db93363d4d
Matthew Hayes <matthewha853@gmail.com>
2026-08-31 21:48:48 -0700

    Consolidate GRDB implementation

--- Sources/SQLiteCross/GRDB.swift
+++ Sources/SQLiteCross/GRDB.swift
@@ -463,4 +463,14 @@
     @usableFromInline
     init() {}
   }
+
+  extension CrossProcessDatabase where Driver == GRDBDatabaseDriver {
+    /// Creates a cross-process database backed by a GRDB database writer.
+    public convenience init(
+      writer: any DatabaseWriter,
+      id: DatabaseIdentifier? = nil
+    ) {
+      self.init(driver: GRDBDatabaseDriver(writer: writer), id: id)
+    }
+  }
 #endif
